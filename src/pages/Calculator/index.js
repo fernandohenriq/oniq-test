2e9472830a8ee70c4a36d4ad1408076b8522a1e7
Fernando Henrique <fernandohenriqsilva@gmail.com>
2020-11-17 00:29:58 -0400

    Add select options from state

--- src/pages/Calculator/index.js
+++ src/pages/Calculator/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Select from 'react-select';
+
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 
@@ -30,58 +31,50 @@ function Calculator(){
   return(
     <>
       <Header />
-      <div id="calc">
-        <p id="text">
-          Veja qual plano <span>FaleMais</span> combina com você, de forma simples, fácil e transparente!
-        </p>
-        <form action="#">
-          <div className="input-group">
-            <label htmlFor="input-plan">Escolha um de nossos planos</label>
-            <select name="input-plan" id="input-plan">
-              <option value="0">FaleMais30</option>
-              <option value="1">FaleMais60</option>
-              <option value="2">FaleMais120</option>
-            </select>
-          </div>
-          <div className="input-group">
-            <label htmlFor="input-plan">DDD de origem</label>
-            <select name="input-plan" id="input-plan">
-              <option value="011">011	São Paulo -	SP</option>
-              <option value="011">011	Aluminio -	SP</option>
-              <option value="016">016	Altinopolis	- SP</option>
-              <option value="016">016	Americo Brasiliense -	SP</option>
-              <option value="017">017	Adolfo	- SP</option>
-              <option value="017">017	Altair -	SP</option>
-              <option value="018">018	Presidente Prudente	- SP</option>
-              <option value="018">018	Presidente Venceslau -	SP</option>
-            </select>
-          </div>
-          <div className="input-group">
-            <label htmlFor="input-plan">DDD de destino</label>
-            <select name="input-plan" id="input-plan">
-              <option value="011">011	São Paulo -	SP</option>
-              <option value="011">011	Aluminio -	SP</option>
-              <option value="016">016	Altinopolis	- SP</option>
-              <option value="016">016	Americo Brasiliense -	SP</option>
-              <option value="017">017	Adolfo	- SP</option>
-              <option value="017">017	Altair -	SP</option>
-              <option value="018">018	Presidente Prudente	- SP</option>
-              <option value="018">018	Presidente Venceslau -	SP</option>
-            </select>
-          </div>
-          <div className="input-group">
-            <label htmlFor="input-minutes">DDD de destino</label>
-            <input type="number" name="input-minutes" id="input-minutes" placeholder="0 min" />
-          </div>
-          <div id="show-value">
-            <p>Você paga:</p>
-            <p>R$ <span>0,00</span></p>
-            <p>Sem um plano: <s>R$ {showValue}</s></p>
-          </div>
-          <div className="input-group">
-            <button onClick="">Peça já o seu plano</button>
-          </div>
-        </form>
+      <div className="container">
+        <div id="calc">
+          <p id="text">
+            Veja qual plano <span>FaleMais</span> combina com você, de forma simples, fácil e transparente!
+          </p>
+          <form action="#">
+            <div className="input-group">
+              <label htmlFor="input-plan">Escolha um de nossos planos</label>
+              <select name="input-plan" id="input-plan">
+                {plans.map((option) => (
+                  <option value={option.value}>{option.label}</option>
+                ))}
+              </select>
+            </div>
+            <div className="input-group">
+              <label htmlFor="input-plan">DDD de origem</label>
+              <select className="select" name="input-plan" id="input-plan">
+                {cities.map((option) => (
+                  <option value={option.value}>{option.label}</option>
+                ))}
+              </select>
+            </div>
+            <div className="input-group">
+              <label htmlFor="input-plan">DDD de destino</label>
+              <select className="select" name="input-plan" id="input-plan">
+                {cities.map((option) => (
+                    <option value={option.value}>{option.label}</option>
+                ))}
+              </select>
+            </div>
+            <div className="input-group">
+              <label htmlFor="input-minutes">Quantidade de minutos</label>
+              <input className="input" type="number" name="input-minutes" id="input-minutes" placeholder="0 min" />
+            </div>
+            <div id="show-value">
+              <p>Você paga:</p>
+              <p>R$ <span>0,00</span></p>
+              <p>Sem um plano: <s>R$ {showValue}</s></p>
+            </div>
+            <div className="input-group">
+              <button onClick="">Peça já o seu plano</button>
+            </div>
+          </form>
+        </div>
       </div>
       <Footer />
     </>
