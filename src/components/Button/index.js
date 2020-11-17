095886ef9cbf3e05b5900f5dae66e329b677c24f
Fernando Henrique <fernandohenriqsilva@gmail.com>
2020-11-17 18:55:28 -0400

    Created button component

---
+++ src/components/Button/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const styles = {
+    minHeight: "5rem",
+    width: "100%",
+    borderRadius: "1.25rem",
+    borderWidth: "0rem",
+    fontSize: "1.5rem",
+    fontWeight: "700",
+    backgroundColor: "#FC6621",
+    color: "#ffffff",
+}
+
+function Button (props) {
+  return <button style={styles} props>{props.children}</button>
+}
+
+export default Button;
