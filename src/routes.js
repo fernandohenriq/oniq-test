f5fc440a2afd43c623739ba19e4ed6328a484a2e
Fernando Henrique <fernandohenriqsilva@gmail.com>
2020-11-18 00:00:25 -0400

    Added login and admin rotes

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,16 @@ import React from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 
 import Calculator from './pages/Calculator';
+import Login from './pages/Login';
+import Admin from './pages/Admin';
 
 function Routes(){
     return(
         <BrowserRouter>
             <Switch>
                 <Route path="/" exact component={Calculator} />
+                <Route path="/login" exact component={Login} />
+                <Route path="/admin" exact component={Admin} />
             </Switch>
         </BrowserRouter>
     );
