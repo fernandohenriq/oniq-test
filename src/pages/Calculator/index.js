28689b8987525d369b83bc6a5faa9e8e0deee345
Fernando Henrique <fernandohenriqsilva@gmail.com>
2020-11-20 04:38:00 -0400

    Added default values ​​for options without update

--- src/pages/Calculator/index.js
+++ src/pages/Calculator/index.js
@@ -9,12 +9,14 @@ import './styles.css'
 function Calculator(){
 
   const plans = [
+    {value: "0", label: "Selecionar", time: 0},
     {value: "1", label: "FaleMais 30", time: 30},
     {value: "2", label: "FaleMais 60", time: 60},
     {value: "3", label: "FaleMais 120", time: 120}
   ];
 
   const cities = [
+    {value: "0", code: "DDD"},
     {value: "1", code: "011"},
     {value: "2", code: "016"},
     {value: "3", code: "017"},
@@ -22,59 +24,61 @@ function Calculator(){
   ];
 
   const codeTable = [
-    {valuee: "1", origin: "011", destiny: "016", price: 1.90},
-    {valuee: "2", origin: "016", destiny: "011", price: 2.90},
-    {valuee: "3", origin: "011", destiny: "017", price: 1.70},
-    {valuee: "4", origin: "017", destiny: "011", price: 2.70},
-    {valuee: "5", origin: "011", destiny: "018", price: 0.90},
-    {valuee: "6", origin: "018", destiny: "011", price: 1.90},
+    {value: "1", origin: "011", destiny: "016", price: 1.90},
+    {value: "2", origin: "016", destiny: "011", price: 2.90},
+    {value: "3", origin: "011", destiny: "017", price: 1.70},
+    {value: "4", origin: "017", destiny: "011", price: 2.70},
+    {value: "5", origin: "011", destiny: "018", price: 0.90},
+    {value: "6", origin: "018", destiny: "011", price: 1.90},
   ];
 
-  const [plan, setPlan] = useState("3");
-  const [origin, setOrigin] = useState("4");
-  const [destiny, setDestiny] = useState("1");
-  const [minutes, setMinutes] = useState(200);
+  const [plan, setPlan] = useState("0");
+  const [origin, setOrigin] = useState("0");
+  const [destiny, setDestiny] = useState("0");
+  const [minutes, setMinutes] = useState(0);
   const [result, setResult] = useState({
     withPlan: '0,00',
     withOutPlan: '0,00'
   })
 
   useEffect(() => {
-    const orig = cities[origin-1].code;
-    const dest = cities[destiny-1].code;
-    const time = plans[plan-1].time
-    const tax = codeTable.find((e) => e.origin === orig && e.destiny === dest);
-
-    const resPlan = () => {
-      if (minutes > time) {
-        return tax ? String((((minutes-time)*tax.price)*1.1).toFixed(2)).replace(".",",") : '-';
-      } else {
-        return '0,00';
+    if (plan !== "0" && origin !== "0" && destiny !== "0" && minutes ) {
+      const orig = cities[origin].code;
+      const dest = cities[destiny].code;
+      const time = plans[plan].time
+      const tax = codeTable.find((e) => e.origin === orig && e.destiny === dest);
+  
+      const resPlan = () => {
+        if (minutes > time) {
+          return tax ? String((((minutes-time)*tax.price)*1.1).toFixed(2)).replace(".",",") : '-';
+        } else {
+          return '0,00';
+        }
       }
+      
+      const resNoPlan = () => {
+        return tax ? String(((minutes)*tax.price).toFixed(2)).replace(".",",") : '-';
+      }
+  
+      setResult({
+        withPlan: resPlan(),
+        withOutPlan: resNoPlan()
+      });
+  
+      const log = {
+        date: new Date().toLocaleString(),
+        origin: orig,
+        destiny: dest,
+        minutes: minutes,
+        plan: plans[plan].label,
+        planPrice: resPlan(),
+        noPlanPrice: resNoPlan()
+      }
+  
+      const dataRef = firebase.database().ref('log');
+      dataRef.push(log);
+      
     }
-    
-    const resNoPlan = () => {
-      return tax ? String(((minutes)*tax.price).toFixed(2)).replace(".",",") : '-';
-    }
-
-    setResult({
-      withPlan: resPlan(),
-      withOutPlan: resNoPlan()
-    });
-
-    const log = {
-      date: new Date().toLocaleString(),
-      origin: orig,
-      destiny: dest,
-      minutes: minutes,
-      plan: plans[plan-1].label,
-      planPrice: resPlan(),
-      noPlanPrice: resNoPlan()
-    }
-
-    const dataRef = firebase.database().ref('log');
-    dataRef.push(log);
-    
   }, [plan, origin, destiny, minutes]);
 
   return(
@@ -90,7 +94,10 @@ function Calculator(){
               <label htmlFor="input-plan">Escolha um de nossos planos</label>
               <select name="input-plan" id="input-plan" value={plan} onChange={e => setPlan(e.target.value)}>
                 {plans.map((option) => (
-                  <option key={option.value} value={option.value}>{option.label}</option>
+                  option.value === "0" ?
+                    <option key={option.value} value={option.value} disabled>{option.label}</option>
+                  :
+                    <option key={option.value} value={option.value} >{option.label}</option>
                 ))}
               </select>
             </div>
@@ -98,7 +105,10 @@ function Calculator(){
               <label htmlFor="input-plan">DDD de origem</label>
               <select className="select" name="input-plan" id="input-plan" value={origin} onChange={e => setOrigin(e.target.value)}>
                 {cities.map((option) => (
-                  <option key={option.value} value={option.value}>{option.code}</option>
+                  option.value === "0" ?
+                    <option key={option.value} value={option.value} disabled>{option.code}</option>
+                  :
+                    <option key={option.value} value={option.value}>{option.code}</option>
                 ))}
               </select>
             </div>
