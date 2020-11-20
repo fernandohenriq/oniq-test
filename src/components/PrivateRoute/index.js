c110c8455afbe7f3009b28339c21fd95b554f781
Fernando Henrique <fernandohenriqsilva@gmail.com>
2020-11-20 03:09:04 -0400

    Reindented route props

--- src/components/PrivateRoute/index.js
+++ src/components/PrivateRoute/index.js
@@ -5,10 +5,7 @@ import { AuthContext } from '../../utils/auth';
 const PrivateRoute = ({ component: RouteComponent, ...rest }) => {
   const { currentUser } = useContext(AuthContext);
   return (
-    <Route
-      {...rest}
-      render={routeProps =>
-        !!currentUser ? (
+    <Route {...rest} render={routeProps => !!currentUser ? (
           <RouteComponent {...routeProps} />
         ) : (
           <Redirect to={"/login"} />
