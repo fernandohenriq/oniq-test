11f56a8462287a812e866a9a3781dbbad7bf3931
Fernando Henrique <fernandohenriqsilva@gmail.com>
2020-11-20 04:31:22 -0400

    Updated table style to be more responsive

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -4,34 +4,36 @@ import React from 'react';
 import './styles.css';
 
 function Table (props) {
-  return <>
-    <table id="table">
-      <thead>
-        <tr>
-          <th>Data/Hora</th>
-          <th>Origem</th>
-          <th>Destino</th>
-          <th>Tempo</th>
-          <th>Plano FaleMais</th>
-          <th>Com FaleMais</th>
-          <th>Sem FaleMais</th>
-        </tr>
-      </thead>
-      <tbody>
-        {props.data.map((e, i) => (
-          <tr key={i}>
-            <td>{e.date}</td>
-            <td>{e.origin}</td>
-            <td>{e.destiny}</td>
-            <td>{e.minutes}</td>
-            <td>{e.plan}</td>
-            <td>$ {e.planPrice}</td>
-            <td>$ {e.noPlanPrice}</td>
+  return <div>
+  <div style={{overflowX: "auto"}}>
+      <table id="table">
+        <thead>
+          <tr>
+            <th>Data/Hora</th>
+            <th>Origem</th>
+            <th>Destino</th>
+            <th>Tempo</th>
+            <th>Plano FaleMais</th>
+            <th>Com FaleMais</th>
+            <th>Sem FaleMais</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
-  </>
+        </thead>
+        <tbody>
+          {props.data.map((e, i) => (
+            <tr key={i}>
+              <td>{e.date}</td>
+              <td>{e.origin}</td>
+              <td>{e.destiny}</td>
+              <td>{e.minutes}</td>
+              <td>{e.plan}</td>
+              <td>$ {e.planPrice}</td>
+              <td>$ {e.noPlanPrice}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  </div>
 }
 
 export default Table;
