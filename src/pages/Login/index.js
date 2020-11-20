126f042d197255bd432159a45f1fcd7ce66b2f05
Fernando Henrique <fernandohenriqsilva@gmail.com>
2020-11-20 01:21:05 -0400

    Login page redone with login firebase auth

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,51 +1,53 @@
-import React, { useState } from 'react';
-import { Link } from "@reach/router";
+import React, { useCallback, useContext } from 'react';
+import { withRouter, Redirect } from 'react-router-dom';
+import firebase from '../../firebase';
+import { AuthContext } from '../../utils/auth';
 
 import Header from '../../components/Header';
 import Button from '../../components/Button';
 
 import './styles.css'
 
-function Admin() {
-
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState(null);
-  const signInWithEmailAndPasswordHandler = 
-          (event,email, password) => {
-              event.preventDefault();
-  };
-
-  const onChangeHandler = (event) => {
-      const {name, value} = event.currentTarget;
-
-      if(name === 'userEmail') {
-          setEmail(value);
+const Login = ({ history }) => {
+  const handleLogin = useCallback(
+    async event => {
+      event.preventDefault();
+      const { email, password } = event.target.elements;
+      try {
+        await firebase.auth().signInWithEmailAndPassword(email.value, password.value);
+        history.push('/admin');
+      } catch (err) {
+        alert(err);
       }
-      else if(name === 'userPassword'){
-        setPassword(value);
-      }
-  };
+    },
+    [history]
+  );
+
+  const { currentUser } = useContext(AuthContext);
+
+  if (currentUser) {
+    return <Redirect to="/admin" />
+  }
 
   return <>
     <Header />
-    <div id="admin">
-      <form action="/admin">
+    <div id="admin" onSubmit={handleLogin}>
+      <form action="/admin" method="POST">
         <i class="fa fa-sign-in" aria-hidden="true"></i>
         <div className="input-group">
-          <label htmlFor="input-email">E-mail</label>
-          <input type="email" name="input-email" id="input-email" value={email} onChange={(e) => {onChangeHandler}} />
+          <label htmlFor="email">E-mail</label>
+          <input type="email" name="email" id="input-email" value="email@email.com" />
         </div>
         <div className="input-group">
-          <label htmlFor="input-pass">Senha</label>
-          <input type="password" name="input-pass" id="input-email" value={password} onChange={(e) => {onChangeHandler}} />
+          <label htmlFor="password">Senha</label>
+          <input type="password" name="password" id="input-email" value="123456"/>
         </div>
         <div className="input-group">
-          <Button>Entrar</Button>
+          <Button type="submit">Entrar</Button>
         </div>
       </form>
     </div>
   </>
 }
 
-export default Admin;
+export default withRouter(Login);
