c98cb48188348ccc6968b2db285e7faedb4e8b24
Fernando Henrique <fernandohenriqsilva@gmail.com>
2020-11-20 01:02:42 -0400

    Added primary and secondary theme

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,18 +1,29 @@
 import React from 'react';
 
-const styles = {
+const secondary = {
+  minHeight: "5rem",
+  width: "100%",
+  borderRadius: "1.25rem",
+  borderWidth: "0rem",
+  fontSize: "1.5rem",
+  fontWeight: "700",
+  backgroundColor: "#FC6621",
+  color: "#ffffff",
+}
+
+const primary = {
     minHeight: "5rem",
     width: "100%",
     borderRadius: "1.25rem",
     borderWidth: "0rem",
     fontSize: "1.5rem",
     fontWeight: "700",
-    backgroundColor: "#FC6621",
+    backgroundColor: "#00416D",
     color: "#ffffff",
 }
 
 function Button (props) {
-  return <button style={styles} >{props.children}</button>
+  return <button style={props.theme==="primary" ? primary : secondary } >{props.children}</button>
 }
 
 export default Button;
